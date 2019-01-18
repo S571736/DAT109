6eb9a653ae23046642c5f1672993eff3f2122316
S571736 <Sondre.Gjesdal@gmail.com>
2019-01-18 12:21:12 +0100

    Made some new classes and started work on some methods

---
+++ StigeSpill/src/no/hvl/DAT109/Brett.java
@@ -0,0 +1,12 @@
+package no.hvl.DAT109;
+
+import java.util.ArrayList;
+
+public class Brett {
+    public ArrayList<Rute> ruter = new ArrayList<Rute>();
+
+
+    public void flyttBrikke(Brikke brikke, int plasser){
+        //TODO
+    }
+}
---
+++ StigeSpill/src/no/hvl/DAT109/StigeSpill.java
@@ -0,0 +1,18 @@
+package no.hvl.DAT109;
+
+import java.util.ArrayList;
+
+public class StigeSpill {
+    public Brett brett;
+    public ArrayList<Spiller> spillere = new ArrayList<Spiller>();
+    public Terning terning;
+
+    public Spiller opprettSpiller() {
+        //TODO
+        return null;
+    }
+
+    public void spillRunde() {
+        //TODO
+    }
+}
